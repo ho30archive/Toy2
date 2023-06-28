6b4cd816a0d3da490ed50f413901d9b73a5edba0
ho30archive <ho30archive@gmail.com>
2023-06-28 21:50:38 +0900

    feat : StadiumDao 구현

--- src/main/java/toy/baseball/management/dao/StadiumDao.java
+++ src/main/java/toy/baseball/management/dao/StadiumDao.java
@@ -1,4 +1,102 @@
 package toy.baseball.management.dao;
 
+import ch.qos.logback.core.joran.spi.NoAutoStartUtil;
+import toy.baseball.management.model.Stadium;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.time.LocalTime.now;
+
 public class StadiumDao {
+    private Connection connection;
+
+    public StadiumDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<Stadium> findAllStadium(){
+        // 0. collection
+        List<Stadium> stadiumList = new ArrayList<>();
+        // 1. sql
+        String query = "select * from stadium_tb";
+
+        // 2. buffer
+        try{
+            PreparedStatement statement = connection.prepareStatement(query);
+
+            // 3. send
+            ResultSet rs = statement.executeQuery();
+
+            // 4. cursor while
+            while (rs.next()) {
+                // 5. mapping(parsing) (db result -> model)
+                Stadium stadium = new Stadium(
+                        rs.getInt("stadium_id"),
+                        rs.getString("stadium_name"),
+                        rs.getTimestamp("created_At")
+                );
+
+                // 5. collect
+                stadiumList.add(stadium);
+            }
+            return stadiumList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void registerStadium(String stadiumName) {
+        LocalDateTime currentDateTime = LocalDateTime.now(); // Get the current date and time
+
+
+        // 1. sql
+        String query = "insert into stadium_tb values (?, ?, ?)";
+
+        // 2. buffer
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, 0);
+            statement.setString(2, stadiumName);
+            statement.setTimestamp(3, Timestamp.valueOf(currentDateTime));
+
+            statement.executeUpdate();
+            System.out.println("등록 완료!");
+        } catch (Exception e) {
+            System.out.println("등록 실패!= " + e.getMessage());
+        }
+    }
+
+    // stadium one
+//    public Stadium findByName(String stadiumName) {
+//        // 1. sql
+//        String query = "select * from stadium_tb where stadium_name = ?";
+//
+//        // 2. buffer
+//        try {
+//            PreparedStatement statement = connection.prepareStatement(query);
+//            statement.setString(1, stadiumName);
+//
+//            // 3. send
+//            ResultSet rs = statement.executeQuery();
+//            if (rs.next()) {
+//                Stadium stadium = new Stadium(
+//                        rs.getInt("stadium_id"),
+//                        rs.getString("stadium_name"),
+//                        rs.getTimestamp("created_At")
+//                );
+//                return stadium;
+//            }
+//
+//            // 4. mapping(parsing) (db result -> model)
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return null;
+//    }
+
 }
