23a98535d5111c37bcbe5b4836fe753b9d39de96
ho30archive <ho30archive@gmail.com>
2023-06-29 21:11:56 +0900

    fix : Player, PlayerDao 수정, todo 추가

--- src/main/java/toy/baseball/management/dao/PlayerDao.java
+++ src/main/java/toy/baseball/management/dao/PlayerDao.java
@@ -221,6 +221,11 @@ public class PlayerDao {
         }
     }
 
+    // TODO: 2023/06/29 퇴출쿼리짜야됨 
+    public void outPlayer() {
+        
+    }
+
 
 
 }
--- src/main/java/toy/baseball/management/model/Player.java
+++ src/main/java/toy/baseball/management/model/Player.java
@@ -1,5 +1,7 @@
 package toy.baseball.management.model;
 
+import toy.baseball.management.enums.Positions;
+
 import java.sql.Timestamp;
 
 public class Player {
@@ -9,4 +11,23 @@ public class Player {
     private String name;
     private String position;
     private Timestamp createdAt;
+
+    public Player(int id, int teamId, String name, String position, Timestamp createdAt) {
+        this.id = id;
+        this.teamId = teamId;
+        this.name = name;
+        this.position = position;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", teamId=" + teamId +
+                ", name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
