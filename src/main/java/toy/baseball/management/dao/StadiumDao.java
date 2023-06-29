41a5bc9417313b4fbb58d000844e1472981c9965
ho30archive <ho30archive@gmail.com>
2023-06-29 14:44:35 +0900

    feat : delete 쿼리 추가

--- src/main/java/toy/baseball/management/dao/StadiumDao.java
+++ src/main/java/toy/baseball/management/dao/StadiumDao.java
@@ -71,32 +71,61 @@ public class StadiumDao {
     }
 
     // stadium one
-//    public Stadium findByName(String stadiumName) {
-//        // 1. sql
-//        String query = "select * from stadium_tb where name = ?";
-//
-//        // 2. buffer
-//        try {
-//            PreparedStatement statement = connection.prepareStatement(query);
-//            statement.setString(1, stadiumName);
-//
-//            // 3. send
-//            ResultSet rs = statement.executeQuery();
-//            if (rs.next()) {
-//                Stadium stadium = new Stadium(
-//                        rs.getInt("id"),
-//                        rs.getString("name"),
-//                        rs.getTimestamp("created_At")
-//                );
-//                return stadium;
-//            }
-//
-//            // 4. mapping(parsing) (db result -> model)
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return null;
-//    }
+    public Stadium findByName(String stadiumName) {
+        // 1. sql
+        String query = "select * from stadium_tb where name = ?";
+
+        // 2. buffer
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, stadiumName);
+
+            // 3. send
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                Stadium stadium = new Stadium(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getTimestamp("created_at")
+                );
+                return stadium;
+            }
+
+            // 4. mapping(parsing) (db result -> model)
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public void deleteStadium(int id) {
+
+        String deleteQuery = "delete from stadium_tb where id = ?";
+        String selectQuery = "select (name) from player_tb where id = ?";
+
+
+        // 2. buffer
+        try {
+            String name = null;
+            PreparedStatement deletePstmt = connection.prepareStatement(deleteQuery);
+            PreparedStatement selectPstmt = connection.prepareStatement(selectQuery);
+
+            deletePstmt.setInt(1, id);
+            selectPstmt.setInt(1, id);
+
+            ResultSet rs = selectPstmt.executeQuery();
+            if (rs.next()) {
+                name = rs.getString("name");
+            }
+
+            deletePstmt.executeUpdate();
+            System.out.println("플레이어 " + name + " 삭제 완료!");
+
+        } catch (Exception e) {
+            System.out.println("삭제 실패!= " + e.getMessage());
+        }
+
+    }
 
 }
